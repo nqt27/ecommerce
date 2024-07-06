fc0e04cbacacd21bbe18f6b61c3ed655f911e1a5
nqt27 <nqt271@gmail.com>
2024-07-06 14:08:22 +0700

    boostrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/home-style.css') }}">
     <title>Document</title>
 </head>
 <body>
+    <div class="container">
     <header>
         <div class="top-header">
             <a href="#"><span>Welcome to website</span></a>
@@ -17,10 +19,9 @@
         <div class="main-header">
             <div class="navbar">
                 <ul>
-                    <li><a href="#" class="logo">MyApp</a></li>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#about">About</a></li>
+                    <li><a href="#home">About us</a></li>
+                    <li><a href="#services">Product</a></li>
+                    <li><a href="#about">Blog</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
@@ -38,5 +39,6 @@
             </div>
         </div>
     </header>
+    </div>
 </body>
 </html>
