1c7e763507d3aaa5e3f46fddc750fe3034106b38
dat015 <datnguyen151004@gmail.com>
2024-07-06 16:02:41 +0700

    footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,8 +41,8 @@
         
         </div>
     </header>
-</body>
-<footer >
+    
+    <footer >
     
     <div class="container">
          <div class="footer-left">
@@ -54,26 +54,47 @@
                     <span>Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và và cản trở việc tập trung vào yếu tố trình bày văn bản</span>
                 </div>
             <div class="social">
-
+                <a href="#">
+                    <div class="social-item">
+                        <i class="bi bi-facebook"></i>
+                    </div>
+                </a>
+              
+                <a href="#">
+                    <div class="social-item">
+                        <i class="bi bi-twitter"></i>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="social-item">
+                        <i class="bi bi-youtube"></i>
+                    </div>
+                </a> <a href="#">
+                    <div class="social-item">
+                        <i class="bi bi-whatsapp"></i>
+                    </div>
+                </a>
             </div>
         </div>
         
         <div class="footer-center">
             <div><h3>CONTACT</h3></div>
             <div>
-                 <strong>Địa Chỉ:</strong> 
+                 <b style="white-space: nowrap;"><i class="bi bi-house"></i> Địa Chỉ:</b> 
                  <span>Số 29 đường số 3, KDC Vĩnh Lộc, P. Bình Hưng Hòa, Q. Bình Tân, Tp. HCM </span> 
             </div>
             <div>
                 
-                <strong><i class="bi bi-telephone-inbound"></i>  Hotline: </strong>
-                <span></span>
+                <b><i class="bi bi-telephone-inbound"></i>  Hotline: </b>
+                <span>0919 797 626 - 0898 800 088</span>
             </div>
             <div>
-                <strong><i class="bi bi-envelope-at"></i> Email: </strong>
+                <b><i class="bi bi-envelope-at"></i> Email: </b>
+                <span>demerite@gmail.com</span>
             </div>
             <div>
-                <strong>Website: </strong>
+                <b><i class="bi bi-globe"></i> Website: </b>
+                <span>demerite.com.vn</span>
             </div>
         </div>
         
@@ -81,8 +102,8 @@
             <div>
                 <h3>FANPAGE</h3>
             </div>
-            <div>
-                
+            <div class="fanpage_img">
+                <img src="{{ asset('image/img_fanpage.png') }}" alt="">
             </div>
         </div>
 
@@ -90,4 +111,6 @@
        
  
 </footer>
+</body>
+
 </html>
