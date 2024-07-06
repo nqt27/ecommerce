d2a39dc7e5dc9eb8c56a34569f0b65975804b6b6
nqt27 <nqt271@gmail.com>
2024-07-06 13:23:26 +0700

    css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,27 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/home-style.css') }}">
     <title>Document</title>
 </head>
 <body>
     <header>
         <div class="top-header">
             <a href="#"><span>Welcome to website</span></a>
+            <span>|</span>
             <a href="#"><span>Subcribe</span></a>
         </div>
         <div class="main-header">
-        <div class="navbar">
-            <ul>
-                <li><a href="#" class="logo">MyApp</a></li>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </div>
-        <div class="logo">
-            <img src="{{ asset('image/logo.png') }}" alt="">
-        </div>
+            <div class="navbar">
+                <ul>
+                    <li><a href="#" class="logo">MyApp</a></li>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+            <div class="logo">
+                <img src="{{ asset('image/logo.png') }}" alt="">
+            </div>
+            <div class="right-header">
+                <div class="search">
+                    <img src="{{ asset('image/research.png') }}" alt="">
+                    <span>SEARCH</span>
+                </div>
+                <div class="cart">
+                    <img src="{{ asset('image/shopping-bag.png') }}" alt="">
+                </div>
+            </div>
         </div>
     </header>
 </body>
