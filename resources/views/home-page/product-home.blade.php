38ca5e0ae5c6ecf490cff6572e5428698a0c8082
dat015 <datnguyen151004@gmail.com>
2024-07-16 16:06:43 +0700

    update product

--- resources/views/home-page/product-home.blade.php
+++ resources/views/home-page/product-home.blade.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/home-style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/.css') }}">
     <link rel="stylesheet" href="{{ asset('css/product-content.css') }}">
     <title>Document</title>
 </head>
 <body>
+    @include('home-page.layout_header')
     <div class="container">
         <div class="content-left">
             <div class="content-left__title-cate">
@@ -137,5 +140,8 @@
         </div>
         
     </div>
+
+    @include('home-page.layout_footer')
+
 </body>
 </html>
