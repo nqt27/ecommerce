3c26b72d3eab58a0babbe2dd0e763ac286363906
nqt27 <nqt271@gmail.com>
2024-07-25 08:21:32 +0700

    fix

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -151,8 +151,8 @@
             <div class="nav-item dropdown">
                 @auth
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                    <img class="rounded-circle me-lg-2" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                    <span class="d-none d-lg-inline-flex">{{ Auth::user()->name }}</span>
+                    <!-- <img class="rounded-circle me-lg-2" src="img/user.jpg" alt="" style="width: 40px; height: 40px;"> -->
+                    <span class="d-none d-lg-inline-flex">Welcome, {{ Auth::user()->name }}!</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                     <a href="#" class="dropdown-item">My Profile</a>
